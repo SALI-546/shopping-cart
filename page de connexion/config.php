6f42e4753156ca41de6a8a1b0a703c81bc776598
mouhamadousalifou <150121971+SALI-546@users.noreply.github.com>
2024-08-08 23:59:00 +0200

    Update config.php

--- page de connexion/config.php
+++ page de connexion/config.php
@@ -1,5 +1,5 @@
 <?php
-	$conn = new mysqli("localhost","root","","cart_system");
+	$conn = new mysqli("localhost","dbusername","dbpassword","dbname");
 	if($conn->connect_error){
 		die("Connection Failed!".$conn->connect_error);
 	}
